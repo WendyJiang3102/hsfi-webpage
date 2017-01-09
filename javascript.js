57540ea936ebee310e2d8079e9d7020c6dc9950b
Wendy Jiang <wjiang.student@hsfi.us>
2017-01-09 22:09:51 +0000

    added content

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// JavaScript File
+$("#home-button").click(function(){
+    $(".all-content").hide();
+    $("#home-content").show();
+});
+
+$("#about-us-button").click(function(){
+    $(".all-content").hide();
+    $("#about-us-content").show();
+});
